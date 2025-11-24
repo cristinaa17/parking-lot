6dd5c83841af788f73ac4134b59c7b9b0374c738
cristinaa17 <cristinamaria.radulescu@ulbsibiu.ro>
2025-11-24 18:04:54 +0200

    created the EJB

--- src/main/java/org/parking/parkinglot/Cars.java
+++ src/main/java/org/parking/parkinglot/Cars.java
@@ -1,16 +1,25 @@
 package org.parking.parkinglot;
 
+import jakarta.inject.Inject;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import org.common.CarDto;
+import org.ejb.CarsBean;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "Cars", value = "/Cars")
 public class Cars extends HttpServlet {
+    @Inject
+    CarsBean carsBean;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
             ServletException, IOException {
+        List<CarDto> cars = carsBean.findAllCars();
+        request.setAttribute("cars", cars);
         request.setAttribute("numberOfFreeParkingSpots", 10);
         request.getRequestDispatcher("/WEB-INF/pages/Cars.jsp").forward(request,response);
     }
